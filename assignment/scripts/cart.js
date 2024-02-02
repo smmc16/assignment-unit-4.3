82068d26adec32a41bf0b2fb0e88de9ff2858587
Sarah McCartney <smmc16@gmail.com>
2024-02-01 20:52:51 -0600

    completed assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,15 +2,65 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+
+function addItem(item) {
+    if (isFull() === false) {
+        basket.push(item);
+        return true;
+    } else {
+        return false;
+    }
+}
+addItem('orange');
+addItem('apple');
+console.log(addItem('milk'));
+addItem('cookies');
+addItem('ice cream');
+console.log(addItem('pizza'));
 
 
+function listItems() {
+    for(let items of basket) {
+        console.log(items);
+    }
+}
 
+listItems();
 
+function empty() {
+    for (let items of basket) {
+        basket.splice(items);
+    }
+}
 
+empty();
+console.log(basket);
 
+const maxItems = 5;
 
+function isFull() {
+    if (basket.length >= 5) {
+        return true;
+    } else if (basket.length < 5) {
+        return false;
+    }
+}
 
+console.log(isFull());
 
+function removeItem(item) {
+    let index = basket.indexOf(item);
+    console.log(index);
+    if (index >= 0) {
+        basket.splice(basket[index]);
+        return item;
+    } else {
+        return null;
+    }
+}
+
+removeItem('apple');
 
 // DO NOT MODIFY
 // Used for automated testing
